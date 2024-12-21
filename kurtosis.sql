5ccda4e80d6b749c00acc21966aa1755718712dd
babak_keshavarz <babak0keshavarz@gmail.com>
2024-12-21 10:37:59 -0800

    added normalized kurtosis definition

--- kurtosis.sql
+++ kurtosis.sql
@@ -1,4 +1,6 @@
+ -- Since the kurtosis of a normal distribution is 3, kurtosis can be defined as kurtosis - 3 to signify its deviation from the normal distribution 
  SELECT
   POW(SUM(Column - AVG), 4) OVER(Group) / (N * POW(STD, 4)) OVER(GROUP) AS KURTOSIS,
+  POW(SUM(Column - AVG), 4) OVER(Group) / (N * POW(STD, 4)) OVER(GROUP) - 3 AS KURTOSIS_NORMALIZED,
  FROM
   INPUT_TABLE
