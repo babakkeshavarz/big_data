d393b1c61463711acc81f4a942c6433595c9f052
babak_keshavarz <babak0keshavarz@gmail.com>
2025-01-09 21:19:32 -0800

    added interpretation of kurtosis

--- kurtosis.sql
+++ kurtosis.sql
@@ -1,5 +1,5 @@
  -- Since the kurtosis of a normal distribution is 3, kurtosis can be defined as kurtosis - 3 to signify its deviation from the normal distribution.
- -- higher kurtosis corresponds to greater extremity of deviations (or outliers)
+ -- Higher kurtosis corresponds to greater extremity of deviations (or outliers). The more positive the value, the more outliers in the data.
  SELECT
   POW(SUM(Column - AVG), 4) OVER(Group) / (N * POW(STD, 4)) OVER(GROUP) AS KURTOSIS,
   POW(SUM(Column - AVG), 4) OVER(Group) / (N * POW(STD, 4)) OVER(GROUP) - 3 AS KURTOSIS_NORMALIZED,
